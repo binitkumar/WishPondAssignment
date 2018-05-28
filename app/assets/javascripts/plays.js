deb648b4aaa0c087cae4991246ee13738aed8552
Binit Kumar <binit.kumar.hbs@gmail.com>
2018-05-28 14:10:18 +0530

    Added image update in table

--- app/assets/javascripts/plays.js
+++ app/assets/javascripts/plays.js
@@ -31,5 +31,16 @@ function captureData(){
   xhttp.setRequestHeader("X-CSRF-Token", getCSRFToken() );
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xhttp.send("number="+ (10 - number_counter) +"&image_url=" + image_collection[image_counter]);
+  
+  row = document.createElement("tr");
+  col1= document.createElement("td");
+  col1.innerHTML = 10 - number_counter;
+  row.appendChild(col1);
+
+  col2= document.createElement("td");
+  col2.innerHTML = "<img src="+ image_collection[image_counter]+" style='width:200px;height:200px'>";
+  row.appendChild(col2);
+  
+  document.getElementById("plays-listing").appendChild( row );
 }
 setInterval(incrementSeconds, 1000);
